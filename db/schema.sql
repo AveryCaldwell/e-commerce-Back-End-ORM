e07a235c704e21ddf3b776228dee2b54883eb4a2
Avery <averycaldwell7@gmail.com>
2023-02-15 17:18:37 -0800

    edit model comments

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,3 @@ CREATE DATABASE ecommerce_db;
 
 
 
-
--- ~~~ Associations ~~~
--- You'll need to execute association methods on your Sequelize models to create the following relationships between them:
--- Product belongs to Category, and Category has many Product models, as a category can have multiple products but a product can only belong to one category.
--- Product belongs to many Tag models, and Tag belongs to many Product models. Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
--- Hint: Make sure you set up foreign key relationships that match the column we created in the respective models.
--- models/index.js
+++ models/index.js
@@ -4,25 +4,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// ** TODO **
-// Products belongsTo Category ~~ COMPLETED
+// Products belongsTo Category 
 Product.belongsTo(Category, {
     foreignKey: 'category_id',
 });
 
-// ** TODO **
-// Categories have many Products ~~ COMPLETED
+// Categories have many Products 
 Category.hasMany(Product, {
     foreignKey: 'product_id',
     onDelete: 'CASCADE',
 });
-// ** TODO **
-// Products belongToMany Tags (through ProductTag) ~~ COMPLETED
+// Products belongToMany Tags (through ProductTag) 
 
 Product.belongsToMany(Tag, { through: 'ProductTag' });
 
-// ** TODO **
-// Tags belongToMany Products (through ProductTag) ~~ COMPLETED
+// Tags belongToMany Products (through ProductTag) 
 Tag.belongsToMany(Product, { through: 'ProductTag' });
 
 module.exports = {
